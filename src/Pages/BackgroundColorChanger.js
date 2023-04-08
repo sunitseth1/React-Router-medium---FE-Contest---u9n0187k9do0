ead39c935f59f0aefc943d456c53ff3828332331
sunit seth <100702483+sunitseth1@users.noreply.github.com>
2023-04-08 20:14:56 +0530

    Update BackgroundColorChanger.js

--- src/Pages/BackgroundColorChanger.js
+++ src/Pages/BackgroundColorChanger.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {userParams} fromn 'react-router-dom';
 
 
 export const BackgroundColorChanger = () =>{
+    let {colorname}=ueParams(
+;
+        console.log(colorname);
     return(
-       
+       <main style={{height:"100vh",backgroundColor:`${colorname}`}}>
+       Background Color Change
+       </main>
     )
 }
